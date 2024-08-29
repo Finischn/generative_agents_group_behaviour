cef529effbe0ae18fcda969a0765b8f1b7ce657a
Finischn <finsc@web.de>
2024-08-29 10:32:22 +0200

    make conversation mode and simulation name accesible

---
+++ reverie/backend_server/config.py
@@ -0,0 +1,13 @@
+# config.py
+class Conv_mode:
+    conversation_mode = "default"
+
+class DescriptionMode:
+    description_mode = "default"
+
+class Simulation_name:
+    simulation_name = ""
+
+conv_mode = Conv_mode()
+prompt_mode = DescriptionMode()
+sim_name= Simulation_name()
